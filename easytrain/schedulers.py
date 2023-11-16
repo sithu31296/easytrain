138c66f05b94811ecff98834020183f1d8ae5cab
sithu31296 <sithu31296@gmail.com>
2023-11-16 14:05:06 +0900

    update scheduler

--- easytrain/schedulers.py
+++ easytrain/schedulers.py
@@ -1,11 +1,12 @@
 import torch
 from torch.utils.data import Dataset, DataLoader
 from torch.optim import Optimizer
-from torch.optim.lr_scheduler import CosineAnnealingLR, LinearLR, ChainedScheduler
+from torch.optim.lr_scheduler import CosineAnnealingLR, LinearLR, ChainedScheduler, MultiStepLR
 from easytrain.utils.distributed import *
 
 
 def create_scheduler(
+    scheduler_name: str,
     optimizer: Optimizer,
     dataset: Dataset,
     dataloader: DataLoader,
@@ -14,6 +15,7 @@ def create_scheduler(
     lr: float,
     end_lr: float = 1e-8,
     warmup_iters: int = 10,
+    milestones: list = None,
 ):
     num_of_gpus = get_world_size()
     iters_per_epoch = len(dataset) // (batch_size * num_of_gpus)
@@ -21,6 +23,14 @@ def create_scheduler(
     warmup_iters = min(warmup_iters, len(dataloader)-1)
 
     scheduler1 = LinearLR(optimizer, lr, total_iters=warmup_iters)
-    scheduler2 = CosineAnnealingLR(optimizer, max_iters, end_lr)
+
+    if scheduler_name == 'multisteplr':
+        if milestones is None:
+            milestones = [int(epochs*0.6)*iters_per_epoch, int(epochs*0.85)*iters_per_epoch]
+        else:
+            milestones = list(map(lambda x: int(x*iters_per_epoch), milestones))
+        scheduler2 = MultiStepLR(optimizer, milestones, gamma=0.1)
+    else:
+        scheduler2 = CosineAnnealingLR(optimizer, max_iters, end_lr)
 
     return ChainedScheduler([scheduler1, scheduler2])
